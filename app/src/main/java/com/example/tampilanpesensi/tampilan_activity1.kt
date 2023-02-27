e22ba35275dcc8d43383cfa9b6a96d90eb93f48c
rzqzdan <rizqizidan26@gmail.com>
2023-02-27 09:59:28 +0700

    finish tampilan

---
+++ app/src/main/java/com/example/tampilanpesensi/tampilan_activity1.kt
@@ -0,0 +1,18 @@
+package com.example.tampilanpesensi
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class tampilan_activity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tampilan1)
+    }
+
+    fun btn1(view: View) {
+       
+    }
+
+}
